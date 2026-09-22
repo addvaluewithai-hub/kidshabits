46b61f67c388099d3636ec5ddb1c062a214503d9
addvaluewithai-hub <addvaluewithai@gmail.com>
2026-09-22 21:44:25 +0300

    Load world v2 presentation styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css';
 import './character-fit.css';
 import './milestone1.css';
 import './milestone2.css';
+import './world-v2.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
