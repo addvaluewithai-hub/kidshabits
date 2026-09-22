04e792f89b899391ec9288cb2bc810497bdd89da
addvaluewithai-hub <addvaluewithai@gmail.com>
2026-09-22 21:22:25 +0300

    Load in-world companion milestone styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './app/App';
 import './styles.css';
 import './character-fit.css';
 import './milestone1.css';
+import './milestone2.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
