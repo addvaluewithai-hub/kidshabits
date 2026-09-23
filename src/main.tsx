188bc4fe043678b50899b90c7690da7a3610be88
addvaluewithai-hub <addvaluewithai@gmail.com>
2026-09-23 05:29:59 +0300

    Load galaxy journey styles

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import './platform.css';
 import './parent-center.css';
 import './visual-foundation.css';
 import './onboarding-polish.css';
+import './galaxy-journey.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
