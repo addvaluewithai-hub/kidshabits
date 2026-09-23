2acca8dae939cf17905fa1bfa5ac32fe680412cf
addvaluewithai-hub <addvaluewithai@gmail.com>
2026-09-23 07:58:20 +0300

    Validate registered world art manifests

--- src/world-runtime/art/registry.ts
+++ src/world-runtime/art/registry.ts
@@ -1,12 +1,18 @@
 import type { WorldId } from '../../worlds/types';
+import { getWorldManifest } from '../../worlds/registry';
 import { moonGardenArtManifest } from '../../worlds/moon-garden/art/manifest';
 import { sproutArtManifest } from '../../worlds/sprout-planet/art/manifest';
 import type { WorldArtManifest } from './types';
+import { assertValidWorldArtManifest } from './validate';
 
 const manifests: Record<WorldId, WorldArtManifest> = {
   sprout: sproutArtManifest,
   'moon-garden': moonGardenArtManifest,
 };
+
+(Object.keys(manifests) as WorldId[]).forEach(worldId => {
+  assertValidWorldArtManifest(manifests[worldId], getWorldManifest(worldId));
+});
 
 export function getWorldArtManifest(worldId: WorldId) {
   return manifests[worldId];
